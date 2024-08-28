656809df4574c96eb04664e3e518985afc5cf035
Oscar Javier Pazos Mogollón <turokoscar@gmail.com>
2024-08-28 11:50:26 -0500

    Se reinicia el proyecto
    Por temas de usabilidad ya no se contara con taildwind css

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# NgTemplate16
+# TemplateInia
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.14.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-template-16'`, () => {
+  it(`should have as title 'template-inia'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ng-template-16');
+    expect(app.title).toEqual('template-inia');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ng-template-16 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('template-inia app is running!');
   });
 });
